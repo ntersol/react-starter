55c57535b7a1a8ea2b4fe8e0474ab70b4069b80e
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2022-06-13 14:11:40 -0400

    Add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+  useEffect(() => {
+    
+  })
+
   return (
     <div className="App">
       <header className="App-header">
