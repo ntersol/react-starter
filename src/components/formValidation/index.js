d0767024ea200e78abb75272f8cb8ed5f57143a7
Greg Sandell <gsandell_99@yahoo.com>
2022-08-30 12:06:47 -0700

    WIP making form look better

--- src/components/formValidation/index.js
+++ src/components/formValidation/index.js
@@ -11,7 +11,7 @@ export default function FormValidation () {
     lastName: ''
   })
 
-  const emailValid = /^.+@.+\..+$/
+  const emailValid = /^.+@.+\..+$/ // Note, this a very simplified email pattern
   const nameValid = /^([^0-9]*)$/
 
   const onSubmit = (data) => {
@@ -28,29 +28,29 @@ export default function FormValidation () {
     <div className={style.main}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Please enter your name and email</h1>
-        <i>Numbers not allowed in name fields</i>
+        <div className={style['name-requirement']}>Numbers not allowed in name fields</div>
         <div className={style.field}>
-          <label>Email</label>
+          <div className={style.label}>Email</div>
           <input {...register('email', { required: true, pattern: emailValid })} />
-          {errors.email?.type === 'required' && <div className={style.err}>This field is required</div>}
-          {errors.email?.type === 'pattern' && <div className={style.err}>Please enter a valid email address</div>}
         </div>
+        {errors.email.type === 'required' && <div className={style.err}>This field is required</div>}
+        {errors.email.type === 'pattern' && <div className={style.err}>Please enter a valid email address</div>}
         <div className={style.field}>
-          <label>First name</label>
+          <div className={style.label}>First name</div>
           <input {...register('firstName', { required: true, pattern: nameValid })} />
-          {errors.firstName?.type === 'required' && <div className={style.err}>This field is required</div>}
-          {errors.firstName?.type === 'pattern' && <div className={style.err}>Numbers are not allowed</div>}
         </div>
+        {errors.firstName.type === 'required' && <div className={style.err}>This field is required</div>}
+        {errors.firstName.type === 'pattern' && <div className={style.err}>Numbers are not allowed</div>}
         <div className={style.field}>
-          <label>Last name</label>
+          <div className={style.label}>Last name</div>
           <input {...register('lastName', { required: true, pattern: nameValid })} />
-          {errors.flastName?.type === 'required' && <div className={style.err}>This field is required</div>}
-          {errors.lastName?.type === 'pattern' && <div className={style.err}>Numbers are not allowed</div>}
         </div>
+        {errors.flastName.type === 'required' && <div className={style.err}>This field is required</div>}
+        {errors.lastName.type === 'pattern' && <div className={style.err}>Numbers are not allowed</div>}
         <input type='submit' />
         <button id={style.cancel} onClick={handleCancel}>Cancel</button>
         {/* Do not use watch() to grab values, as they will change as user types in the already-submitted form */}
-        {submitted && (<div>
+        {submitted && (<div className={style.submitted}>
           <div>Submitted Name: {submittedForm.firstName} {submittedForm.lastName}</div>
           <div>Submitted Email: {submittedForm.email}</div>
         </div>
