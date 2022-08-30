18fa1f6c525e6894eaa4cd7ca08559bbecdf8bf6
Greg Sandell <gsandell_99@yahoo.com>
2022-08-30 15:07:58 -0700

    Restore missing option chaining operator "?"; pin eslint version to exactly 8.22.0

--- src/components/formValidation/index.js
+++ src/components/formValidation/index.js
@@ -33,20 +33,20 @@ export default function FormValidation () {
           <div className={style.label}>Email</div>
           <input {...register('email', { required: true, pattern: emailValid })} />
         </div>
-        {errors.email.type === 'required' && <div className={style.err}>This field is required</div>}
-        {errors.email.type === 'pattern' && <div className={style.err}>Please enter a valid email address</div>}
+        {errors.email?.type === 'required' && <div className={style.err}>This field is required</div>}
+        {errors.email?.type === 'pattern' && <div className={style.err}>Please enter a valid email address</div>}
         <div className={style.field}>
           <div className={style.label}>First name</div>
           <input {...register('firstName', { required: true, pattern: nameValid })} />
         </div>
-        {errors.firstName.type === 'required' && <div className={style.err}>This field is required</div>}
-        {errors.firstName.type === 'pattern' && <div className={style.err}>Numbers are not allowed</div>}
+        {errors.firstName?.type === 'required' && <div className={style.err}>This field is required</div>}
+        {errors.firstName?.type === 'pattern' && <div className={style.err}>Numbers are not allowed</div>}
         <div className={style.field}>
           <div className={style.label}>Last name</div>
           <input {...register('lastName', { required: true, pattern: nameValid })} />
         </div>
-        {errors.flastName.type === 'required' && <div className={style.err}>This field is required</div>}
-        {errors.lastName.type === 'pattern' && <div className={style.err}>Numbers are not allowed</div>}
+        {errors.lastName?.type === 'required' && <div className={style.err}>This field is required</div>}
+        {errors.lastName?.type === 'pattern' && <div className={style.err}>Numbers are not allowed</div>}
         <input type='submit' />
         <button id={style.cancel} onClick={handleCancel}>Cancel</button>
         {/* Do not use watch() to grab values, as they will change as user types in the already-submitted form */}
