dc9c92b91ab58dd264e816a9b9fbeac8de1617ad
Greg Sandell <gsandell_99@yahoo.com>
2022-08-31 13:20:49 -0700

    <ReduxDemo> code cleanup

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,14 +1,10 @@
 import { SET_GLOBAL_STATE } from '../types'
 export const setGlobalState = (state) => (dispatch, getState) => {
-  console.log(`setGlobalState invoked with ${JSON.stringify(state)}`)
   try {
     dispatch({
       type: SET_GLOBAL_STATE,
       payload: {
-        ...state,
-        message: `Update client ${// TODO remove if not used
-          getState().ourReducer.client
-        } to ${state} `
+        ...state
       }
     })
   } catch (error) {
