e46b62e31a5155aa126ee9cec5ac0c9770cba2c0
gsandell_FirstAm <gsandell@firstam.com>
2022-09-12 16:27:12 -0700

    Add missing typescript parser to lint configuration; migrate one component to typescript and provide an interface for its props

---
+++ src/components/context1/index.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import cx from 'classnames'
+import StarterContext from '../../containers/App/StarterContext.js'
+import { IContextProps } from '../../interfaces'
+export default function Context1 (props:IContextProps) {
+  const { JSONdata, serviceRenderer } = useContext(StarterContext)
+
+  return (
+    <div className={cx(props.style.contextPanel, props.style.red)}>
+      <h1 className={props.style.red}>Component Context1</h1>
+      <p>Component <span className={props.style.green}>Context2</span> is my sibling.  I got this data from the Context API rather than a prop!</p>
+      {serviceRenderer(JSONdata)}
+    </div>
+  )
+}
