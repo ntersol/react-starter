5f62290ab30e4bebf70dec150f48ade7b237d777
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2023-04-13 10:08:38 -0400

    Finalize css nav mobile behavior

--- src/components/masterpage/nav/nav.tsx
+++ src/components/masterpage/nav/nav.tsx
@@ -1,13 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import styles from './nav.module.scss';
 import { useState } from 'react';
+import classNames from 'classnames';
 
 export function Nav() {
   const [isMobileNavOpen, setMobileNavOpen] = useState(false);
 
   return (
     <div id={styles['nav-main']}>
-      <nav className="d-none d-lg-flex">
+      <nav className={classNames('d-none d-lg-flex')}>
         <NavLink to="/users" className={styles['nav-menu-link']}>
           User Management
         </NavLink>
@@ -18,9 +19,12 @@ export function Nav() {
           Redux Demo
         </NavLink>
       </nav>
-      <div className={styles['nav-toggler']}>
-        <div className="pt-2 float-end d-block d-lg-none">
-          <a>Toggle!</a>
+      <div className={classNames(styles['nav-end'], 'd-flex d-md-block')}>
+        <div className="d-none d-lg-block">Utility Nav!</div>
+        <div className={styles['nav-toggler']}>
+          <div className="pt-2 float-end d-block d-lg-none">
+            <a>Toggle!</a>
+          </div>
         </div>
       </div>
     </div>
