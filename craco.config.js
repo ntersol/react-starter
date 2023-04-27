776840ad0d66bd79513702d822107681e479236b
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2023-04-27 09:53:01 -0400

    Fix tsconfig error with pathing

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,9 @@
 /* craco.config.js */
 const path = require('path');
 
-/** React TS by default does not support paths, this adds webpack support for them */
+/** React TS by default does not support paths, this adds webpack support for them.
+ * These paths need to match those in the tsconfig file and vice versa. TS does not see this file and will throw errors otherwise
+ */
 module.exports = {
   webpack: {
     alias: {
